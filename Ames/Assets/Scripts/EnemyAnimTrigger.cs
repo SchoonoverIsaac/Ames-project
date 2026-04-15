5d8a4ba9ec59466a8a77a56c23430c19f4b53109
ABasicCoder <darriensengmayphet274@gmail.com>
2026-04-15 11:50:36 -0700

    Just some adjustments

---
+++ Ames/Assets/Scripts/EnemyAnimTrigger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemyAnimTrigger : MonoBehaviour
+{
+    anim = getcomponent(Animator)();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the Enemy is staying still, play idle animation
+        if (getcomponent"Enemy"
+    }
+}
--- Ames/Assets/Scripts/EnemyNavMove.cs
+++ Ames/Assets/Scripts/EnemyNavMove.cs
@@ -7,12 +7,14 @@ public class EnemyNavMove : MonoBehaviour
     NavMeshAgent agent;
     public float chaseDistance = 10;
     private Vector3 home;
+    bool m_idle
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         agent = GetComponent<NavMeshAgent>();
         home = transform.position;
+        m_Animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,10 +24,25 @@ public class EnemyNavMove : MonoBehaviour
         if (direction.magnitude < chaseDistance)
         {
             agent.destination = player.transform.position;
+            //this is where it is moving
+        }
+        else if(home != null) 
+        {        
+            agent.destination = home;
+            //here too
+        }
+        if
+        {
+            //we're stopped, so play the idle animation
+            m_idle = true;
         }
         else
         {
-            agent.destination = home;
+            m_jump = false;
         }
+        if (m_idle == false)
+            m_Animator.SetBool("idle", false);
+        if (m_idle == true)
+            m_Animator.SetBool("idle", true);
     }
 }
