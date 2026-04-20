7db95b8be2f0ca68fcc60a90396e47343d6e31a4
ABasicCoder <darriensengmayphet274@gmail.com>
2026-04-20 12:50:52 -0700

    Some test stuff

---
+++ Ames/Assets/Scripts/EnemyNavMove.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavMove : MonoBehaviour
+{
+    GameObject player;
+    NavMeshAgent agent;
+    public float chaseDistance = 10;
+    private Vector3 home;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+        home = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        if (direction.magnitude < chaseDistance)
+        {
+            agent.destination = player.transform.position;
+            GetComponent<Animator>().SetBool("Walk", true);
+        }
+        else
+        {
+            agent.destination = home;
+            GetComponent<Animator>().SetBool("Walk", false);
+        }
+    }
+}
