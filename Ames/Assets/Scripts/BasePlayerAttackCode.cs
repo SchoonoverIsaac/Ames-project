1559db9a539609c0ab15eda4b37d2ca4318c5355
Mikogano <26.noah.misek@ksd.org>
2026-04-22 13:41:25 -0700

    Made Base Attack code

--- Ames/Assets/Scripts/BasePlayerAttackCode.cs
+++ Ames/Assets/Scripts/BasePlayerAttackCode.cs
@@ -3,19 +3,19 @@ using UnityEngine.InputSystem;
 
 public class BasePlayerAttackCode : MonoBehaviour
 {
-public bool SpellMode = false;
-public bool BowMode = false;
-public bool SwordMode = true;
-public GameObject SwordPrefab;
-public float SwingSpeed = 0.1f;
-public float SwingLifetime = 2f;
-public GameObject SpellcastPrefab;
-public float SpellTravelSpeed = 10f;
-public float SpellLifetime = 2f;
-public GameObject ArrowPrefab;
-public float shootSpeed = 10f;
-public float ArrowLifetime = 2f;
-    
+    public bool SpellMode = false;
+    public bool BowMode = false;
+    public bool SwordMode = true;
+    public GameObject SwordPrefab;
+    public float SwingSpeed = 0.1f;
+    public float SwingLifetime = 2f;
+    public GameObject SpellcastPrefab;
+    public float SpellTravelSpeed = 10f;
+    public float SpellLifetime = 2f;
+    public GameObject ArrowPrefab;
+    public float shootSpeed = 10f;
+    public float ArrowLifetime = 2f;
+
     public bool projectileShoot = true;
     public GameObject prefab;
     public Transform spawnPosition;
@@ -24,7 +24,7 @@ public float ArrowLifetime = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
     public void OnToggle1(InputValue value)
     {
@@ -33,8 +33,8 @@ public float ArrowLifetime = 2f;
         BowMode = false;
         SwordMode = true;
 
-}
-    
+    }
+
     public void OnShoot(InputValue value)
     {
         //add your 3D shooting code here
@@ -73,10 +73,82 @@ public float ArrowLifetime = 2f;
                     SpellcastPrefab.GetComponent<Rigidbody>().linearVelocity = velocity * SpellTravelSpeed;
                     Destroy(SpellcastPrefab, SpellLifetime);
                 }
+
+                if (SwordMode == true)
+                {
+                    //first cast the ray out from the camera, in the way it is looking
+                    //this variable will store info of what we hit, if anything
+                    RaycastHit hit2;
+                    Ray ray2 = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+                    //if we hit something, tell me what we hit
+                    if (Physics.Raycast(ray2, out hit2, 10) && !projectileShoot)
+                    {
+                        if (hit2.collider != null)
+                        {
+                            Debug.Log(hit2.collider.gameObject.name);
+                            if (hit2.collider.gameObject.GetComponent<EnemyHealth>() != null)
+                            {
+                                hit2.collider.gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //this is where we want to spawn the projectile
+                        //our preferred destination will be the point where our raycast hits
+                        Vector3 dest = hit.point;
+                        if (hit.collider == null)
+                        {
+                            dest = Camera.main.transform.position + Camera.main.transform.forward * SwingSpeed;
+                        }
+                        GameObject SwordPrefab = Instantiate(prefab, spawnPosition.position, Quaternion.identity);
+                        Vector3 velocity = dest - spawnPosition.position;
+                        velocity.Normalize();
+                        SwordPrefab.GetComponent<Rigidbody>().linearVelocity = velocity * SwingSpeed;
+                        Destroy(SwordPrefab, SwingLifetime);
+                    }
+                    if (BowMode == true)
+                    {
+                        //first cast the ray out from the camera, in the way it is looking
+                        //this variable will store info of what we hit, if anything
+                        RaycastHit hit1;
+                        Ray ray1 = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+                        //if we hit something, tell me what we hit
+                        if (Physics.Raycast(ray1, out hit1, 10) && !projectileShoot)
+                        {
+                            if (hit1.collider != null)
+                            {
+                                Debug.Log(hit1.collider.gameObject.name);
+                                if (hit1.collider.gameObject.GetComponent<EnemyHealth>() != null)
+                                {
+                                    hit1.collider.gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            //this is where we want to spawn the projectile
+                            //our preferred destination will be the point where our raycast hits
+                            Vector3 dest = hit.point;
+                            if (hit.collider == null)
+                            {
+                                dest = Camera.main.transform.position + Camera.main.transform.forward * shootSpeed;
+                            }
+                            GameObject ArrowPrefab = Instantiate(prefab, spawnPosition.position, Quaternion.identity);
+                            Vector3 velocity = dest - spawnPosition.position;
+                            velocity.Normalize();
+                            ArrowPrefab.GetComponent<Rigidbody>().linearVelocity = velocity * shootSpeed;
+                            Destroy(ArrowPrefab, ArrowLifetime);
+                        }
+                    }
+                }
             }
-            
         }
     }
+                
+            
+        
+    
 
             
     public void OnToggle2(InputValue value)
